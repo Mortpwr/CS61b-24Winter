f5d73003ecda307e8cde3f03e58297d72a1fca18
Mortpwr <mortforg@foxmail.com>
2025-01-15 12:18:00 +0800

    Lab1 1/15

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,10 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    public static boolean isLeapYear(int year) {
+        /** @source Judge which year is LeapYear.*/
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
