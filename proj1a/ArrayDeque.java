6a7552630757e1d561ab807c80b1851bcf4628a6
Mortpwr <mortforg@foxmail.com>
2025-04-17 22:15:46 +0800

    proj1 Completed!

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,69 +3,84 @@ public class ArrayDeque<T> {
     private int size = 8;
     private int first = 0;
     private int last = 0;
-    private T[] initialarrays = (T[]) new Object[size];
-    public ArrayDeque(){}
+    private T[] initialarrays = (T[]) new Object[this.size];
+
+    public ArrayDeque() {
+    }
+
     private void resize() {
         int dsize = this.size * 2;
         T[] newarraylist = (T[]) new Object[dsize];
-        for(int i = 0; i < this.nowsize; i++){
+        for (int i = 0; i < this.nowsize; i++) {
             newarraylist[(this.first + i) % dsize] = this.initialarrays[(first + i) % this.size];
         }
+        this.size = dsize;
     }
+
     public void addFirst(T item) {
-        if(this.nowsize == this.size) {
+        if (this.nowsize == this.size) {
             this.resize();
         }
-        this.first--;
+        if ((this.first != 0 || this.last != 0) && (this.nowsize != 0)) {
+            this.first--;
+        }
         this.initialarrays[this.first % this.size] = item;
         this.nowsize++;
     }
+
     public void addLast(T item) {
-        if(this.nowsize == this.size) {
+        if (this.nowsize == this.size) {
             this.resize();
         }
-        this.last++;
+        if (this.last != 0 || this.first != 0 && (this.nowsize != 0)) {
+            this.last++;
+        }
         this.initialarrays[this.last % this.size] = item;
         this.nowsize++;
     }
+
     public boolean isEmpty() {
-       return this.nowsize == 0;
+        return this.nowsize == 0;
     }
+
     public int size() {
         return this.nowsize;
     }
+
     public void printDeque() {
-        for(int i = 0; i < this.nowsize; i++) {
+        for (int i = 0; i < this.nowsize; i++) {
             System.out.print(this.initialarrays[(this.first + i) % this.size] + " ");
         }
     }
-    private boolean pointerHelper(){
-       return this.first >= this.last;
+
+    private boolean pointerHelper() {
+        return this.nowsize == 0;
     }
-    public T removeFirst() {// may oversize
-        if(this.pointerHelper()) {
+
+    public T removeFirst() {
+        if (this.pointerHelper()) {
             return null;
-        }
-        else{
+        } else {
             T item = this.initialarrays[this.first % this.size];
             this.initialarrays[this.first % size] = null;
-            this.first++;
             this.nowsize--;
+            this.first++;
             return item;
         }
     }
-    public T removeLast() { // may oversize
-       if(this.pointerHelper()) {
-           return null;
-       }
-       else{
+
+    public T removeLast() {
+        if (this.pointerHelper()) {
+            return null;
+        } else {
             T item = this.initialarrays[this.last % this.size];
             this.initialarrays[this.last % this.size] = null;
-            this.last--;
             this.nowsize--;
+            this.last--;
             return item;
-       }
+        }
     }
+
     public T get(int index) {
         return this.initialarrays[(this.first + index) % this.size];
     }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,42 +1,49 @@
 public class LinkedListDeque<T> {
     private int size;
     private ItemNode head = new ItemNode();
+
     public LinkedListDeque() {
         this.size = 1;
     }
-    public void addFirst(T item){
+
+    public void addFirst(T item) {
         ItemNode newNode = new ItemNode(item, head, head.next);
         head.next.prev = newNode;
         head.next = newNode;
         this.size++;
     }
-    public void addLast(T item){
+
+    public void addLast(T item) {
         ItemNode newNode = new ItemNode(item, head.prev, head);
         head.prev.next = newNode;
         head.prev = newNode;
         this.size++;
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return this.size == 1;
     }
-    public int size(){
+
+    public int size() {
         return this.size - 1;
     }
-    private void printHelper(int length, ItemNode head){
-        if(length > 0){
-            System.out.print(head.item + " ");
-            printHelper(length - 1, head.next);
+
+    private void printHelper(int length, ItemNode first) {
+        if (length > 0) {
+            System.out.print(first.item + " ");
+            printHelper(length - 1, first.next);
         }
     }
-    public void printDeque(){
+
+    public void printDeque() {
         ItemNode first = this.head.next;
         printHelper(this.size - 1, first);
     }
-    public T removeFirst(){
-        if(this.isEmpty()){
+
+    public T removeFirst() {
+        if (this.isEmpty()) {
             return null;
-        }
-        else {
+        } else {
             T item = this.head.next.item;
             ItemNode second = this.head.next.next;
             this.head.next = second;
@@ -45,24 +52,25 @@ public class LinkedListDeque<T> {
             return item;
         }
     }
-    public T removeLast(){
-        if(this.isEmpty()) {
-           return null;
-        }
-       else {
+
+    public T removeLast() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
             T item = this.head.prev.item;
             ItemNode second = this.head.prev.prev;
             this.head.prev = second;
             second.next = this.head;
             this.size--;
             return item;
-       }
+        }
     }
-    public T get(int index){
+
+    public T get(int index) {
         ItemNode first = this.head.next;
 
-        while(index + 1 <= this.size && index >= 0){
-            if(index == 0){
+        while (index + 1 <= this.size && index >= 0) {
+            if (index == 0) {
                 return first.item;
             }
             first = first.next;
@@ -70,31 +78,35 @@ public class LinkedListDeque<T> {
         }
         return null;
     }
-    private T getRecursiveHelper(ItemNode node, int index){
-        if(index == 0){
+
+    private T getRecursiveHelper(ItemNode node, int index) {
+        if (index == 0) {
             return node.item;
-        }
-        else{
+        } else {
             return getRecursiveHelper(node.next, index - 1);
         }
     }
-    public T getRecursive(int index){
+
+    public T getRecursive(int index) {
         ItemNode first = this.head.next;
         return getRecursiveHelper(first, index);
     }
-    private class ItemNode{
-       public ItemNode prev;
-       public ItemNode next;
-       public T item;
-       public ItemNode(T item, ItemNode prev, ItemNode next){
-           this.item = item;
-           this.prev = prev;
-           this.next = next;
-       }
-       public ItemNode(){
-           this.item = null;
-           this.prev = this;
-           this.next = this;
-       }
+
+    private class ItemNode {
+        private ItemNode prev;
+        private ItemNode next;
+        private T item;
+
+        public ItemNode(T item, ItemNode prev, ItemNode next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public ItemNode() {
+            this.item = null;
+            this.prev = this;
+            this.next = this;
+        }
     }
 }
